9582d35f3d8d110e72fdd044c73b071680be2185
[Mareeya] <[skninelsh.ya@rmutsvmail.com]>
2020-03-16 22:45:28 +0700

    maya

---
+++ lib/screen/iot_model.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+class IotModel{
+  int led1,mode,sw1,fan1,air1;
+  String namestr,pirtest;
+
+  IotModel(
+    this.led1,this.mode,this.air1,this.fan1,this.sw1,this.namestr,this.pirtest
+  );
+
+    IotModel.formMap(Map<dynamic,dynamic>map){
+      mode = map['mode'];
+      led1 = map['led1'];
+      sw1=map['sw1'];
+      fan1=map['fan1'];
+      air1=map['air1'];
+      namestr=map['namestr'];
+      pirtest=map['pirTest'];
+      
+    }
+
+    Map<dynamic,dynamic> toMap(){
+      final Map<dynamic,dynamic> map = Map<dynamic,dynamic>();
+      map['led1']=led1;
+      map['mode']=mode;
+      map['sw1']=sw1;
+      map['fan1']=fan1;
+      map['air1']=air1;
+      map['namestr']=namestr;
+      map['pirTest']=pirtest;
+
+
+      return map;
+    }
+}
